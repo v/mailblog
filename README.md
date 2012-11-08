6a2c1bcc1f4929f8dd030a3fd3a2d1e867238f2b
Vaibhav Verma <vaibhav2614@gmail.com>
2012-11-08 13:57:56 -0800

    Started the script. Wrote code and a couple tests

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a project that uses the [SendGrid](http://sendgrid.com) Parse API to create a mailing list archive for a Google Group.
+
+I've created a dummy SendGrid email address, and pointed to
+http://domain.com/callback
---
+++ config.py
@@ -0,0 +1,10 @@
+# config
+
+class Configuration(object):
+    DATABASE = {
+        'name': 'emails.db',
+        'engine': 'peewee.SqliteDatabase',
+        'check_same_thread': False,
+    }
+    DEBUG = True
+    SECRET_KEY = 'EPIUWYE)WYE&*W&TEFSGDSUHSTU(PWA^*WATGFESDIUASHDK:PD'
